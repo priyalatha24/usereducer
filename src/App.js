f0004786c23ae58a00ae17d50787d1a0c2b1b082
Priyalatha <priyalatha24042002@gmail.com>
2024-01-20 10:45:39 +0530

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState,useReducer } from 'react';
 import './App.css';
 
+function callback(oldState,mydispatchAction){
+  if(mydispatchAction.type123 === "change_name"){
+    return {
+      name:"pavya",age:oldState.age
+    };
+  }
+  else if(mydispatchAction.type123 === "change_age"){
+    return{
+      name:oldState.name,age:80
+    };
+
+  }
+  else if(mydispatchAction.type123 === "change_input_name"){
+    return{
+      name:mydispatchAction.payload,age:80
+    };
+
+  }
+
+  return oldState;
+}
+
 function App() {
+  const [mystate,mydispatch] = useReducer(callback,{name:"priya",age:22})
+  // let[name,setName] =useState("priya")
+  // let[age,setAge] =useState("33")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{mystate.name}/{mystate.age}</p>
+      <button type ='submit' 
+      onClick={()=>{
+        mydispatch({"type123":"change_name"})}}> Click</button>
+      <button type ='submit' 
+      onClick={()=>{
+        mydispatch({"type123":"change_age"})}}> Age</button> 
+
+      <input type='text' placeholder='Name' onChange={(event)=>{mydispatch({"type123":"change_input_name",payload:event.target.value})}}></input>
+      <input type='text' placeholder='Age' onChange={(event)=>{mydispatch({"type123":"change_input_age",payload:event.target.value})}}></input>
     </div>
   );
 }
